0286fe762c7cb04a75388a055dda01c0b09d3b79
croe <crow.uc@gmail.com>
2017-03-04 04:50:14 +0900

    add layout & artists

--- src/js/App.js
+++ src/js/App.js
@@ -2,8 +2,10 @@ import $ from 'jquery';
 import setUA from './util/setUaBodyClass';
 import Util from './Util.js';
 
+import Map from './_map';
+
 // Application entry point
 $(()=>{
   setUA();
-  
+  Map();
 });
